8cd90e44310c81221186c1b599dae042c99f3475
Kirill Gladkih <kirill.gladkih@personik.com>
2020-10-10 16:30:59 +0300

    feat: Верстка для чата

--- server.js
+++ server.js
@@ -4,10 +4,8 @@ const path = require('path')
 const app = express()
 const PORT = 4000
 
-app.use(express.static(path.join(__dirname, 'dist')))
-
-app.use('/static', express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'static')))
 
 app.listen(PORT, function() {
-	console.log(`Example app listening on port ${PORT}!`)
+	console.log(`App listening on port ${PORT}!`)
 })
